8b1e8e25cb6e9b3e9c15b3d24732676ee2b642a3
dasphotiasht <dasphotiasht@gmail.com>
2023-04-27 18:13:50 +0700

    [Keymap] Added dasphotiasht annepro2/c18 keymap

---
+++ keyboards/annepro2/keymaps/dasphotiasht/config.h
@@ -0,0 +1,1 @@
+#define MOUSEKEY_MAX_SPEED 5
---
+++ keyboards/annepro2/keymaps/dasphotiasht/keymap.c
@@ -0,0 +1,91 @@
+#include QMK_KEYBOARD_H
+#include "ap2_led.h"
+
+enum anne_pro_layers {
+    _BASE_LAYER,
+    _FUNCTION_LAYER,
+    _MOUSE_LAYER,
+    _MEDIA_AND_NAVIGATION_LAYER,
+    _ARROW_LAYER
+};
+
+enum {
+    CAPS_TAP_DANCE,
+    ESC_GRV_TAP_DANCE
+};
+
+const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_BASE_LAYER] = LAYOUT_60_ansi( /* BASE LAYER */
+        TD(ESC_GRV_TAP_DANCE), KC_1,    KC_2,                          KC_3,   KC_4,             KC_5,    KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,       KC_EQL,  KC_BSPC,
+        KC_TAB,                KC_Q,    KC_W,                          KC_E,   KC_R,             KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC,       KC_RBRC, KC_BSLS,
+        TD(CAPS_TAP_DANCE),    KC_A,    KC_S,                          KC_D,   KC_F,             KC_G,    KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,       KC_ENT,
+        KC_LSFT,               KC_Z,    KC_X,                          KC_C,   KC_V,             KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_UP),
+        KC_LCTL,               KC_LGUI, LM(_FUNCTION_LAYER, MOD_LALT), KC_SPC, MO(_ARROW_LAYER), KC_LEFT, KC_DOWN, KC_RIGHT
+    ),
+    [_FUNCTION_LAYER] = LAYOUT_60_ansi( /* FN LAYER */
+        _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_DEL,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______
+    ),
+    [_MOUSE_LAYER] = LAYOUT_60_ansi( /* MOUSE CONTROL LAYER */
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, KC_WH_U, KC_MS_U, KC_WH_D, KC_BTN1, _______, KC_BTN1, KC_WH_U, KC_MS_U, KC_WH_D, _______, _______, _______, _______,
+        _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN2, _______, KC_BTN2, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+        _______, _______, _______, _______, _______, _______, _______, _______
+    ),
+    [_ARROW_LAYER] = LAYOUT_60_ansi( /* WASD AS ARROW LAYER*/
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, KC_UP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+        _______, _______, _______, _______, _______, _______, _______, _______
+    ),
+};
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    switch (get_highest_layer(state)) {
+        case _BASE_LAYER:
+            ap2_led_set_foreground_color(0x00, 0x00, 0x00);
+            break;
+        case _FUNCTION_LAYER:
+            ap2_led_set_foreground_color(0x00, 0xFF, 0x00);
+            break;
+        case _MOUSE_LAYER:
+            ap2_led_set_foreground_color(0x00, 0x88, 0xFF);
+            break;
+        case _ARROW_LAYER:
+            ap2_led_set_foreground_color(0x7A, 0x7A, 0x00);
+            break;
+        default:
+            ap2_led_set_foreground_color(0x00, 0x00, 0x00);
+            break;
+        }
+    return state;
+}
+
+void toggle_layer(uint8_t layer) {
+    if (layer_state_is(layer)) {
+        layer_off(layer);
+    } else {
+        layer_on(layer);
+    }
+}
+
+void handle_caps_tap_dance(tap_dance_state_t *state, void *user_data) {
+    switch (state->count) {
+        case 1:
+            toggle_layer(_MOUSE_LAYER);
+            break;
+        case 2:
+            toggle_layer(_ARROW_LAYER);
+            break;
+    }
+}
+
+tap_dance_action_t tap_dance_actions[] = {
+  [CAPS_TAP_DANCE] = ACTION_TAP_DANCE_FN(handle_caps_tap_dance),
+  [ESC_GRV_TAP_DANCE] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_GRV)
+};
