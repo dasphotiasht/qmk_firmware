c2d9f930cc453f429c5f3e088686ef9365a30cfc
dasphotiasht <dasphotiasht@gmail.com>
2023-05-09 11:34:19 +0700

    [Keymap] use TG instead of TO function_layer

--- keyboards/annepro2/keymaps/dasphotiasht/keymap.c
+++ keyboards/annepro2/keymaps/dasphotiasht/keymap.c
@@ -27,7 +27,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,                KC_Q,    KC_W,                          KC_E,   KC_R,                KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC,       KC_RBRC, KC_BSLS,
         TD(CAPS_TAP_DANCE),    KC_A,    KC_S,                          KC_D,   KC_F,                KC_G,    KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,       KC_ENT,
         KC_LSFT,               KC_Z,    KC_X,                          KC_C,   KC_V,                KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_UP),
-        KC_LCTL,               KC_LGUI, LM(_FUNCTION_LAYER, MOD_LALT), KC_SPC, TO(_FUNCTION_LAYER), KC_LEFT, KC_DOWN, KC_RIGHT
+        KC_LCTL,               KC_LGUI, LM(_FUNCTION_LAYER, MOD_LALT), KC_SPC, TG(_FUNCTION_LAYER), KC_LEFT, KC_DOWN, KC_RIGHT
     ),
     [_MOUSE_LAYER] = LAYOUT_60_ansi( /* MOUSE CONTROL LAYER */
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
