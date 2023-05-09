aaf2345d40a83449ce2483eb18d0285a17355823
dasphotiasht <dasphotiasht@gmail.com>
2023-05-09 16:40:16 +0700

    [Keymap]: LM LGUI to control_layer

--- keyboards/annepro2/keymaps/dasphotiasht/keymap.c
+++ keyboards/annepro2/keymaps/dasphotiasht/keymap.c
@@ -24,11 +24,11 @@ combo_t key_combos[COMBO_COUNT] = {
 
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE_LAYER] = LAYOUT_60_ansi( /* BASE LAYER */
-        TD(ESC_GRV_TAP_DANCE),        KC_1,    KC_2,                          KC_3,   KC_4,                KC_5,    KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,       KC_EQL,  KC_BSPC,
-        KC_TAB,                       KC_Q,    KC_W,                          KC_E,   KC_R,                KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC,       KC_RBRC, KC_BSLS,
-        TD(CAPS_TAP_DANCE),           KC_A,    KC_S,                          KC_D,   KC_F,                KC_G,    KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,       KC_ENT,
-        KC_LSFT,                      KC_Z,    KC_X,                          KC_C,   KC_V,                KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_UP),
-        LM(_CONTROL_LAYER, MOD_LCTL), KC_LGUI, LM(_FUNCTION_LAYER, MOD_LALT), KC_SPC, TG(_FUNCTION_LAYER), KC_LEFT, KC_DOWN, KC_RIGHT
+        TD(ESC_GRV_TAP_DANCE),        KC_1,                         KC_2,                          KC_3,   KC_4,                KC_5,    KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,       KC_EQL,  KC_BSPC,
+        KC_TAB,                       KC_Q,                         KC_W,                          KC_E,   KC_R,                KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC,       KC_RBRC, KC_BSLS,
+        TD(CAPS_TAP_DANCE),           KC_A,                         KC_S,                          KC_D,   KC_F,                KC_G,    KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,       KC_ENT,
+        KC_LSFT,                      KC_Z,                         KC_X,                          KC_C,   KC_V,                KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_UP),
+        LM(_CONTROL_LAYER, MOD_LCTL), LM(_CONTROL_LAYER, MOD_LGUI), LM(_FUNCTION_LAYER, MOD_LALT), KC_SPC, TG(_FUNCTION_LAYER), KC_LEFT, KC_DOWN, KC_RIGHT
     ),
     [_MOUSE_LAYER] = LAYOUT_60_ansi( /* MOUSE CONTROL LAYER */
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
